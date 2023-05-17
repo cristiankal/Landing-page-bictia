19207677628f05a2dbbe415ae4c7d21cf94a8fc0
cristian <tunaaltasuba@gmail.com>
2023-05-16 20:50:31 -0500

    landing

---
+++ index.js
@@ -0,0 +1,9 @@
+const form=document.getElementById("form");//traigo el contenedor del formulario 
+
+const nombre = document.getElementById("nombre");//traigo el input de nombre
+const email = document.getElementById("email");
+const asunto= document.getElementById("asunto");
+const mensaje= document.getElementById("mensaje");//traigo el textarea
+
+const enviar= document.getElementById("enviar"); //traigo el boton
+
